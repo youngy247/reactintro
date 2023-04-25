07891f7615aa04603bad30b57f9b3316b1b0b9a5
Adam Young <adamyoungy678@gmail.com>
2023-04-25 15:01:07 +0100

    displayed header component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Greeting from "./Greeting";
+import Header from "./components/Header";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Greeting />
+    <Header />
   </React.StrictMode>
 );
 
