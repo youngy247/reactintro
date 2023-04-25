c3cabc95dfd2940ee13425e0a6dea987cab47aa6
Adam Young <adamyoungy678@gmail.com>
2023-04-25 15:33:28 +0100

    Added button to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import './header.css'
 import Nav from "./Nav";
+import Button from "../Button";
 const Header = () => {
     return (
         <header>
     <h1>Website Header</h1>
             <Nav />
+            <Button />
         </header>
     )
 }
