5a21a0cbfd5d040b63160770d2d4eb986f73aaf7
Adam Young <adamyoungy678@gmail.com>
2023-04-25 15:35:10 +0100

    Added button to content

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,3 +1,12 @@
-const Content = () => <p>Some stuff</p>
+import Button from "../Button";
+
+const Content = () => {
+    return (
+        <main>
+        <p>Some stuff</p>
+            <Button />
+        </main>
+    )
+}
 
 export default Content
