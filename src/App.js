fb7be9872310502fd930ae98162fcaf8f1defd65
Adam Young <adamyoungy678@gmail.com>
2023-04-26 12:06:19 +0100

    deleted unused imports in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
-import CoinToss from "./components/CoinToss/CoinToss";
-import List from "./components/List";
 import Button from "./components/Button";
 
 const App = () => {
