8497173e88905e8d4df026c6e698ff736e0ea604
Adam Young <adamyoungy678@gmail.com>
2023-04-27 10:08:42 +0100

    removed comment

--- src/components/Bitcoin/index.js
+++ src/components/Bitcoin/index.js
@@ -1,24 +1,7 @@
 import {useEffect, useState} from "react";
 
-
 const Bitcoin = () => {
     const [price, setPrice] = useState(0)
-
-    // useEffect(() => {
-    //     const fetchData = () => {
-    //         fetch('https://api.coindesk.com/v1/bpi/currentprice/gbp.json')
-    //             .then(res => res.json())
-    //             .then(data => {
-    //                 setPrice(data.bpi.GBP.rate)
-    //             })
-    //     }
-    //     fetchData()
-    //     // const intervalId = setInterval(fetchData, 3000);
-    //     //
-    //     // return () => clearInterval(intervalId);
-    // }, [])
-
-
 
    const fetchData = () => {
        fetch('https://api.coindesk.com/v1/bpi/currentprice/gbp.json')
