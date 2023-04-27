958e902eec15b72d8fa806f914d75baf05ebd307
Adam Young <adamyoungy678@gmail.com>
2023-04-27 10:08:12 +0100

    updated the btc price every 30 seconds

--- src/components/Bitcoin/index.js
+++ src/components/Bitcoin/index.js
@@ -4,20 +4,42 @@ import {useEffect, useState} from "react";
 const Bitcoin = () => {
     const [price, setPrice] = useState(0)
 
+    // useEffect(() => {
+    //     const fetchData = () => {
+    //         fetch('https://api.coindesk.com/v1/bpi/currentprice/gbp.json')
+    //             .then(res => res.json())
+    //             .then(data => {
+    //                 setPrice(data.bpi.GBP.rate)
+    //             })
+    //     }
+    //     fetchData()
+    //     // const intervalId = setInterval(fetchData, 3000);
+    //     //
+    //     // return () => clearInterval(intervalId);
+    // }, [])
 
-useEffect(() => {
-    fetch('https://api.coindesk.com/v1/bpi/currentprice/gbp.json')
-        .then(res => res.json())
-        .then(data => {
-            setPrice(data.bpi.GBP.rate)
-        })
+
+
+   const fetchData = () => {
+       fetch('https://api.coindesk.com/v1/bpi/currentprice/gbp.json')
+           .then(res => res.json())
+           .then(data => {
+               setPrice(data.bpi.GBP.rate)
+           }).catch((error) => {
+           console.log(error);
+       });
+   }
+
+    useEffect(() => {
+        fetchData()
+        const intervalId = setInterval(fetchData, 30000);
+        return () => clearInterval(intervalId);
 }, [])
-
 
         return (
             <div>
                 <p>
-                    The current Bitcoin price in GBP is: {price}
+                    The current Bitcoin price in GBP is: <strong>{price}</strong>
                 </p>
             </div>
         )
